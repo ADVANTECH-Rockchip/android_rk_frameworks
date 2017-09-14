3b8f854d923a476772e150b662ec852fae8c2f17
zengwei18488 <wei.zeng@advantech.com.cn>
2017-09-14 14:50:29 +0800

    no suspend

--- base/packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ base/packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -2587,7 +2587,7 @@ class DatabaseHelper extends SQLiteOpenHelper {
             loadSetting(stmt, Settings.Global.STAY_ON_WHILE_PLUGGED_IN,
                     ("1".equals(SystemProperties.get("ro.kernel.qemu")) ||
                         mContext.getResources().getBoolean(R.bool.def_stay_on_while_plugged_in))
-                     ? 1 : 0);
+                     ? 1 : 1);
 
             loadIntegerSetting(stmt, Settings.Global.WIFI_SLEEP_POLICY,
                     R.integer.def_wifi_sleep_policy);
