e3660395149927fb65e70e3b7e4993c25751a2b6
zengwei18488 <wei.zeng@advantech.com.cn>
2019-03-29 11:19:18 +0800

    added missing file for contorl nvgbar

--- base/packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
+++ base/packages/SystemUI/src/com/android/systemui/statusbar/tv/TvStatusBar.java
@@ -73,6 +73,10 @@ public class TvStatusBar extends BaseStatusBar {
     @Override
     public void setSystemUiVisibility(int vis, int mask) {
     }
+    
+    @Override
+    public void showNavigationBar() {
+    }
 
     @Override
     public void topAppWindowChanged(boolean visible) {
